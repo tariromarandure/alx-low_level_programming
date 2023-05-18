81279f46f47326b7f59eb4ad16d8153035a8f81c
tariromarandure <tariromarandure2@gmail.com>
2023-05-18 12:53:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * REturn: 0 (Success)
+ */.
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzlel");
+return (0);
+}
